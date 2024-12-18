3bfbf1792ae398724e764ce57c68517d06c6b739
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-18 14:34:57 +0700

    Menambah route file upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/', function () {
+//     return view('fileupload');
+// });
+
+
+Route::get('file-upload', [UploadController::class, 'index']);
+Route::post('upload', [UploadController::class, 'store']);
